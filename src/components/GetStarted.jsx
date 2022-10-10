398c410673cf1dae9eef67208d912d9e8b8df940
ljllanosl <ljllanosl91@gmail.com>
2022-10-10 01:04:46 -0500

    testimonials & clients

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../style";
 import { arrowUp } from "../assets";
 
+// Make the get started button grow on hover
 const GetStarted = () => (
   <div
     className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
